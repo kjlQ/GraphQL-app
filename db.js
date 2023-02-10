c2e36f72a9f756c2ad445b874282cae81f12bf93
Roman <rett8716@gmail.com>
2023-02-10 09:26:55 +0200

    first commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 module.exports = {
   todos: [
-    { id: 1, title: "Lorem Ipsum", user_id: 123, completed: false },
-    { id: 2, title: "Sic Dolor amet", user_id: 456, completed: false },
+    { id: 1, title: "Lorem Ipsum", user_id: 1, completed: false },
+    { id: 2, title: "Sic Dolor amet", user_id: 2, completed: false },
   ],
   users: [
     { id: 123, name: "John Doe" },
